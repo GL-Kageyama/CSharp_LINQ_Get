c17d1da4ac5a0da6042bf3e9f6cf478d37fda000
影山榮一 <a1kageyama@kageyamBookpuro.AirPort>
2021-05-21 14:56:53 +0900

    Update

--- CSharp_LINQ_Get.cs
+++ CSharp_LINQ_Get.cs
@@ -5,92 +5,94 @@ using System.Collections.Generic;
 
 namespace LINQ_Get
 {
-	class MainClass 
-	{
-		public static void Main(string[] args)
-		{
-			var GetObj = new GetClass();
+    class MainClass 
+    {
+        public static void Main(string[] args)
+        {
+            var getClass = new GetClass();
 
-			GetObj.GetArray();
+            getClass.GetArray();
 
-			GetObj.GetList();
-		}
-	}
+            getClass.GetList();
+        }
+    }
 
-	class GetClass
-	{
-		public void GetArray()
-		{
-			var source = new[] {2, 3, 1, 7, 8, 9, 28, 98};
+    class GetClass
+    {
+        public void GetArray()
+        {
+            var source = new[] {2, 3, 1, 7, 8, 9, 28, 98};
 
-			// ElementAt()
-			// 指定したIndexの値を取得
-			Console.WriteLine(source.ElementAt(3));
+            // ElementAt()
+            // Get the value of the specified index
+            Console.WriteLine(source.ElementAt(3));
 
-			// ElementAtOrDefault()
-			// 指定したIndexの値を取得、範囲外の場合は規定値を取得
-			Console.WriteLine(source.ElementAtOrDefault(100));
+            // ElementAtOrDefault()
+            // Get the value of the specified index, or get the specified value if it is out of range
+            Console.WriteLine(source.ElementAtOrDefault(100));
 
-			// First()
-			// 最初の要素を取得
-			Console.WriteLine(source.First());
+            // First()
+            // Get the first element
+            Console.WriteLine(source.First());
 
-			// First()
-			// 10より大きい最初の要素を取得
-			Console.WriteLine(source.First(s => s > 10));
+            // First()
+            // Get the first element greater than 10
+            Console.WriteLine(source.First(s => s > 10));
 
-			// Last()
-			// 最後の要素を取得
-			Console.WriteLine(source.Last());
+            // Last()
+            // Get the last element
+            Console.WriteLine(source.Last());
 
-			// Last()
-			// 20より小さい最後の要素を取得
-			Console.WriteLine(source.Last(s => s < 20));
+            // Last()
+            // Get the last element that is less than 20
+            Console.WriteLine(source.Last(s => s < 20));
 
-			// Single()
-			// 唯一の要素を取得(該当したものが唯一の時のみ取得可)
-			Console.WriteLine(source.Single(s => s < 2));
-		}
+            // Single()
+            // Retrieve the only element (only possible if it is the only one that applies)
+            Console.WriteLine(source.Single(s => s < 2));
 
-		public void GetList()
-		{
-			List<string> Pre = new List<string>
-			{
-				"Kanagawa", "Kyoto", "Gihu", "Okayama", "Yamaguchi", 
-				"Aomori", "Nagano", "Okinawa", "Saitama", "Iwate", 
-			};
+            Console.WriteLine();
+        }
 
-			List<int> integer = new List<int> 
-			{
-				22, 34, 22, 67, 56, 98, 65, 78, 22, 67, 98, 98, 98
-			};
+        public void GetList()
+        {
+            List<string> state = new List<string>
+            {
+                "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", 
+                "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", 
+            };
 
-			// Where()
-			// 該当するものを全て取得
-			IEnumerable<string> query = Pre.Where(x => x.Length > 5);
-			foreach (string ken in query)
-				Console.WriteLine(ken);
+            List<int> intList = new List<int> 
+            {
+                22, 34, 22, 67, 56, 98, 65, 78, 22, 67, 98, 98, 98
+            };
 
-			// Distinct()
-			// 重複を取り除いたものを取得
-			IEnumerable<int> distinctInt = integer.Distinct();
-			foreach (int intOut in distinctInt)
-				Console.WriteLine(intOut);
-			Console.WriteLine();
+            // Where()
+            // Obtain all that apply
+            IEnumerable<string> query = state.Where(x => x.Length > 5);
+            foreach (string ken in query)
+                Console.WriteLine(ken);
+            Console.WriteLine();
 
-			// Skip()
-			// 指定した数をスキップ
-			IEnumerable<int> skipInt = integer.Skip(8);
-			foreach (int intOut in skipInt)
-				Console.WriteLine(intOut);
-			Console.WriteLine();
+            // Distinct()
+            // Get duplicates removed
+            IEnumerable<int> distinctInt = intList.Distinct();
+            foreach (int intOut in distinctInt)
+                Console.WriteLine(intOut);
+            Console.WriteLine();
 
-			// Take()
-			// 先頭から指定した要素を取得
-			IEnumerable<int> takeInt = integer.Take(6);
-			foreach (int intOut in takeInt)
-				Console.WriteLine(intOut); 
-		}
-	}
+            // Skip()
+            // Skip a specified number
+            IEnumerable<int> skipInt = intList.Skip(8);
+            foreach (int intOut in skipInt)
+                Console.WriteLine(intOut);
+            Console.WriteLine();
+
+            // Take()
+            // Get the specified element from the top
+            IEnumerable<int> takeInt = intList.Take(6);
+            foreach (int intOut in takeInt)
+                Console.WriteLine(intOut); 
+        }
+    }
 }
-
